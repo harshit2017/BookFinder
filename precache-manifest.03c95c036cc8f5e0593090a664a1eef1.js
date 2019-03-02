6503e690856e4830462c4f1a10a148f15ea59f5d
Harshit Katyal <harshit1121998@gmail.com>
2019-03-02 19:13:58 +0530

    Updates

---
+++ precache-manifest.03c95c036cc8f5e0593090a664a1eef1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4cc848b538580780aa52b50a7a18e1fa",
+    "url": "/BookFinder/static/media/giphy.4cc848b5.gif"
+  },
+  {
+    "revision": "9ab14a012a3ebdd80f5f",
+    "url": "/BookFinder/static/js/runtime~main.9ab14a01.js"
+  },
+  {
+    "revision": "eb820332bdf8db70b6b4",
+    "url": "/BookFinder/static/js/main.eb820332.chunk.js"
+  },
+  {
+    "revision": "5479adfd83de7848d0cf",
+    "url": "/BookFinder/static/js/2.5479adfd.chunk.js"
+  },
+  {
+    "revision": "eb820332bdf8db70b6b4",
+    "url": "/BookFinder/static/css/main.8bb00104.chunk.css"
+  },
+  {
+    "revision": "7b60ede619e952106ec7d64c1b202136",
+    "url": "/BookFinder/index.html"
+  }
+];
